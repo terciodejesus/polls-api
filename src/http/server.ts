c451345c2d5ab8068834b81150487e159e5d23b0
Tércio Souza de Jesus <terciodejesus@gmail.com>
2024-02-07 00:20:30 -0300

    feat: include votes creatting route

--- src/http/server.ts
+++ src/http/server.ts
@@ -1,25 +1,19 @@
-import { PrismaClient } from '@prisma/client';
+import cookie from '@fastify/cookie';
 import fastify from 'fastify';
-import { z } from 'zod';
+import { createPoll } from './routes/create-poll';
+import { getPoll } from './routes/get-poll';
+import { voteOnPoll } from './routes/vote-on-poll';
 
 const app = fastify()
-const prisma = new PrismaClient()
 
-app.post('/polls', async (request, reply) => {
-  const createPollBody = z.object({
-    title: z.string(),
-  })
-
-  const { title } = createPollBody.parse(request.body)
-
-  const poll = await prisma.poll.create({
-    data: {
-      title
-    }
-  })
-
-  return reply.status(201).send({ pollId: poll.id })
+app.register(cookie, {
+  secret: "D60vzzxsQj6vWuGA6kbHeKr6yRb3931fJtpsDDEZFFc=",
+  hook: 'onRequest',
 })
+
+app.register(createPoll)
+app.register(getPoll)
+app.register(voteOnPoll)
 
 app.listen({ port: 3333 }).then(() => {
   console.log("HTTP server is running on port 3333.")
